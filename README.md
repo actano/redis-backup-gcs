eb336e549cc3a0686398367f87d50f178d4e3e9f
Erick Pereira da Silva <eriiickps@gmail.com>
2019-08-12 14:03:53 +0200

    PA-1041: first commit 🎉
    docker image witch expose a backup script responsable for connect to a redis server, fetch the dump, zip and send it to GCS

---
+++ README.md
@@ -0,0 +1,17 @@
+# redis-backup-gcs
+
+Create a backup of a specified redis db to Google Cloud Storage
+
+## Configuration
+
+Set the following environment variables for the Docker container:
+
+`REDIS_HOST` Hostname of redis db
+
+`REDIS_PORT` Port of redis db
+
+`GCS_BUCKET_REDIS` Google Cloud Storage bucket name
+
+`BACKUP_NAME` Name of the backup file, will be appended by the current date
+
+`GOOGLE_APPLICATION_CREDENTIALS` Path to mounted credentials file (google service account key json file)
---
+++ backup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+if [ -z $REDIS_HOST ] ; then
+    echo "You must specify a REDIS_HOST env var"
+    exit 1
+fi
+if [ -z $REDIS_PORT ] ; then
+    echo "You must specify a REDIS_PORT env var"
+    exit 1
+fi
+
+if [ -z $GCS_BUCKET_REDIS ]; then
+    echo "You must specify a google cloud storage GCS_BUCKET_REDIS address such as gs://my-backups/"
+    exit 1
+fi
+
+if [ -z $BACKUP_NAME ]; then
+    BACKUP_NAME=redis_backup
+fi
+
+CURRENT_DATE=$(date -u +"%Y-%m-%dT%H%M%SZ")
+BACKUP_SET="$BACKUP_NAME-$CURRENT_DATE.rdb"
+
+echo "Activating google credentials before beginning"
+gcloud auth activate-service-account --key-file "$GOOGLE_APPLICATION_CREDENTIALS"
+
+if [ $? -ne 0 ] ; then
+    echo "Credentials failed; no way to copy to google."
+    echo "Ensure GOOGLE_APPLICATION_CREDENTIALS is appropriately set."
+fi
+
+echo "=============== Redis Backup ==============================="
+echo "Beginning backup from $REDIS_HOST to /backup/$BACKUP_SET"
+echo "To google storage bucket $GCS_BUCKET_REDIS using credentials located at $GOOGLE_APPLICATION_CREDENTIALS"
+echo "============================================================"
+
+redis-cli -h ${REDIS_HOST} -p ${REDIS_PORT} --rdb /backup/$BACKUP_SET
+
+echo "Backup size:"
+du -hs "/backup/$BACKUP_SET"
+
+echo "Tarring -> data/$BACKUP_SET.tar"
+tar -cvf "/backup/$BACKUP_SET.tar" "data/$BACKUP_SET"
+
+echo "Zipping -> data/$BACKUP_SET.tar.gz"
+gzip -9 "/backup/$BACKUP_SET.tar"
+
+echo "Zipped backup size:"
+du -hs "/backup/$BACKUP_SET.tar.gz"
+
+echo "Pushing /backup/$BACKUP_SET.tar.gz -> $GCS_BUCKET_REDIS"
+gsutil cp "/backup/$BACKUP_SET.tar.gz" "$GCS_BUCKET_REDIS"
+
+echo "Redis backups ended"
+exit $?
